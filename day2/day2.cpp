69f40d40d55e088aaaa4cc88bddcce867d6cbc17
elevin72 <elilevin11@gmail.com>
2020-12-06 01:10:13 +0200

    day2part2

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -1,45 +1,82 @@
 #include <fstream>
+#include <pthread.h>
 #include <string>
 #include <iostream>
 using namespace std;
 
 const char dash = '-', space = ' ', colon = ':', newLine = '\n';
 
-int day2() {
-    int valid = 0, charCount, min, max, pos;
+struct parsedtext {
+    int min;
+    int max;
     char match;
-    string s; 
+    string password;
+};
+
+parsedtext parseText(ifstream& in) {
+    parsedtext p;
+    string util;
+    getline(in, util, dash);
+    p.min = stoi(util);
+    getline(in, util, space);
+    p.max = stoi(util);
+    getline(in, util, colon);
+    p.match = util[0];
+    in.ignore();
+    getline(in, util, newLine);
+    p.password = util;
+    return p;
+}
+
+int day2p1() {
+    int valid = 0, charCount; 
+    parsedtext p;
     ifstream in ("data.txt");
     if (in.is_open()){
-        for (int i = 0; i < 1000; ++i) {
-            getline(in, s, dash);
-            min = stoi(s);
-            getline(in, s, space);
-            max = stoi(s);
-            getline(in, s, colon);
-            match = s[0]; //hopefully??
-            in.ignore(); // ignores 1 character
-            getline(in, s, newLine);
-            // line is now parsed into bits that I care about
-            // min and max are what you think
-            // match is the character to match
-            // s is the 'password'
+        for(int i = 0; i < 1000; ++i) {
+
+            p = parseText(in);
 
             charCount = 0;
-            for (char c : s) {
-                if (c == match)
+            for (char c : p.password) { //I fricken love modern c++
+                if (c == p.match)
                     ++charCount;
             }
 
-            if (charCount >= min && charCount <= max)
+            if (charCount >= p.min && charCount <= p.max)
                 valid++;
         }
+
         in.close();
     }
+
+    return valid;
+}
+
+int day2p2() {
+    int valid = 0, charCount; 
+    parsedtext p;
+    ifstream in ("data.txt");
+    if (in.is_open()){
+        for(int i = 0; i < 1000; ++i) {
+
+            p = parseText(in);
+
+            charCount = 0;
+
+            if ((p.match == p.password[p.min-1] && p.match != p.password[p.max-1])
+                    || (p.match != p.password[p.min-1] && p.match == p.password[p.max-1] ))  // this is an ugly XOR
+                ++valid;
+        }
+
+        in.close();
+    }
+
     return valid;
 }
 
 int main () {
-    cout << day2() << "\n";
+    cout << day2p1() << "\n";
+    cout << day2p2() << "\n";
     return 0;
 }
