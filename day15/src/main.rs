5a34fa701e44844e2a98f3d014b4984bbe130004
Eli <elilevin11@gmail.com>
2021-12-15 20:20:50 +0200

    a star worked!

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp::Ordering;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashSet};
 use std::fmt;
+use std::hash::{Hash, Hasher};
 
 fn main() {
     println!("{}", star29());
@@ -12,32 +13,46 @@ const DIRS: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)]; // row, col
 struct State<'a> {
     row: usize,
     col: usize,
-    cost_from_start: u32,
+    f: u32,
+    g: u32,
+    h: u32,
     graph: &'a Vec<Vec<u32>>,
 }
 
 impl<'a> State<'a> {
-    fn next_states(&self) -> Vec<State> {
-        let mut next = vec![];
-        for (dy, dx) in DIRS {
-            let new_y = dy + self.row as i32;
-            let new_x = dx + self.col as i32;
-            let len = self.graph.len() as i32;
-            if new_y >= 0 && new_y < len && new_x >= 0 && new_x < len {
-                let new_cost = self.cost_from_start + self.graph[new_y as usize][new_x as usize];
-                next.push(State {
-                    row: new_y as usize,
-                    col: new_x as usize,
-                    cost_from_start: new_cost,
-                    graph: self.graph,
-                });
-            }
+    fn new(row: usize, col: usize, g: u32, graph: &Vec<Vec<u32>>) -> State {
+        let h = State::manhattan_distance(row, col, graph);
+        let f = g + h;
+        State {
+            row,
+            col,
+            f,
+            g,
+            h,
+            graph,
         }
-        next
     }
+    // fn next_states(&self) -> Vec<State> {
+    //     let mut next = vec![];
+    //     for (dy, dx) in DIRS {
+    //         let new_y = dy + self.row as i32;
+    //         let new_x = dx + self.col as i32;
+    //         let len = self.graph.len() as i32;
+    //         if new_y >= 0 && new_y < len && new_x >= 0 && new_x < len {
+    //             let new_cost = self.g + self.graph[new_y as usize][new_x as usize];
+    //             next.push(State {
+    //                 row: new_y as usize,
+    //                 col: new_x as usize,
+    //                 g: new_cost,
+    //                 graph: self.graph,
+    //             });
+    //         }
+    //     }
+    //     next
+    // }
 
-    fn manhattan_distance(&self) -> u32 {
-        ((self.graph.len() - self.row) + (self.graph[0].len() - self.col)) as u32
+    fn manhattan_distance(row: usize, col: usize, graph: &Vec<Vec<u32>>) -> u32 {
+        ((graph.len() - row) + (graph[0].len() - col)) as u32
     }
 }
 
@@ -46,9 +61,9 @@ impl<'a> State<'a> {
 // instead of a max-heap.
 impl<'a> Ord for State<'a> {
     fn cmp(&self, other: &Self) -> Ordering {
-        let a = self.cost_from_start + self.manhattan_distance();
-        let b = other.cost_from_start + other.manhattan_distance();
-        a.cmp(&b)
+        let a = self.g + State::manhattan_distance(self.row, self.col, self.graph);
+        let b = other.g + State::manhattan_distance(other.row, other.col, other.graph);
+        b.cmp(&a)
     }
 }
 
@@ -59,41 +74,81 @@ impl<'a> PartialOrd for State<'a> {
     }
 }
 
+impl<'a> Hash for State<'a> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.row.hash(state);
+        self.col.hash(state);
+    }
+}
+
 impl<'a> fmt::Display for State<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "(row: {}, col: {}, cost: {})",
-            self.row, self.col, self.cost_from_start
+            "row: {}, col: {}, cost: {}, heuristic: {}",
+            self.row,
+            self.col,
+            self.g,
+            State::manhattan_distance(self.row, self.col, self.graph)
         )
     }
 }
 
+fn next_states<'a>(current: State<'a>, graph: &'a Vec<Vec<u32>>) -> Vec<State<'a>> {
+    let mut next = vec![];
+    for (dy, dx) in DIRS {
+        let new_y = dy + current.row as i32;
+        let new_x = dx + current.col as i32;
+        let len = current.graph.len() as i32;
+        if new_y >= 0 && new_y < len && new_x >= 0 && new_x < len {
+            let new_cost = current.g + graph[new_y as usize][new_x as usize];
+            next.push(State::new(new_y as usize, new_x as usize, new_cost, graph));
+        }
+    }
+    next
+}
+
 fn a_star((start_y, start_x): (usize, usize), graph: &Vec<Vec<u32>>) -> u32 {
-    let end_y = graph.len();
-    let end_x = graph[0].len();
+    let end_y = graph.len() - 1;
+    let end_x = graph[0].len() - 1;
 
     let mut frontier = BinaryHeap::<State>::new();
+    let mut visited = HashSet::new();
 
-    let mut current_state = State {
-        row: start_y,
-        col: start_x,
-        cost_from_start: 0,
-        graph,
-    };
+    let mut current_state = State::new(start_y, start_x, 0, graph);
 
     frontier.push(current_state);
 
-    while (current_state.row, current_state.col) != (end_y, end_x) {
-        println!("{}", current_state);
-        for next_state in current_state.next_states() {
-            frontier.push(next_state);
-        }
-        current_state = frontier.pop().unwrap()
-    }
+    while !frontier.is_empty() {
+        current_state = frontier.pop().unwrap();
+        visited.insert(current_state);
 
-    current_state.cost_from_start
+        if (current_state.row, current_state.col) == (end_y, end_x) {
+            return current_state.g;
+        }
+
+        println!("\ncurrent state: ({})", current_state);
+
+        for next_state in next_states(current_state, graph) {
+            if let Some(n) = frontier
+                .iter()
+                .find(|s| s.row == next_state.row && s.col == next_state.col)
+            {
+                if next_state.g < n.g {
+                    frontier = frontier
+                        .into_iter()
+                        .filter(|s| !(s.row == next_state.row && s.col == next_state.col))
+                        .collect();
+                    frontier.push(next_state);
+                }
+            } else {
+                frontier.push(next_state);
+            }
+        }
+    }
+    panic!("damnit")
 }
+
 fn star29() -> u32 {
     let test_map: Vec<Vec<u32>> = Vec::from([
         vec![1, 1, 6, 3, 7, 5, 1, 7, 4, 2],
